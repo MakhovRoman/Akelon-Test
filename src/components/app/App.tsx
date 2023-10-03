2976ad8b64665b799fa22de12150efa5e7aa24f2
Roman Makhov <nekromantos30888666@gmail.com>
2023-10-03 22:41:44 +0300

    feat: add aliases, scss init base components

---
+++ src/components/app/App.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export const App = () => {
+  return (
+    <main>
+      <Typography gutterBottom variant='h2'>Profile Card</Typography>
+    </main>
+  )
+}
